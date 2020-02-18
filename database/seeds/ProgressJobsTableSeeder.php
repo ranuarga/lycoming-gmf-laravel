b2b70bfc9e71502942a8bcc648e19dcd9a8c5619
ranuarga <august.arga@gmail.com>
2020-02-18 20:15:55 +0700

    Update: Edit ProgressJob Seeder

--- database/seeds/ProgressJobsTableSeeder.php
+++ database/seeds/ProgressJobsTableSeeder.php
@@ -14,7 +14,7 @@ class ProgressJobsTableSeeder extends Seeder
     public function run()
     {
         $arr = [];
-        for ($i=1; $i <= 9; $i++) { 
+        for ($i=1; $i <= 10; $i++) { 
             array_push($arr, [
                 'job_id' => 1,
                 'job_sheet_id' => $i,
@@ -39,7 +39,7 @@ class ProgressJobsTableSeeder extends Seeder
             ]);
         }
 
-        for ($i=1; $i <= 9; $i++) { 
+        for ($i=1; $i <= 10; $i++) { 
             if($i < 5){
                 array_push($arr, [
                     'job_id' => 2,
@@ -49,6 +49,30 @@ class ProgressJobsTableSeeder extends Seeder
                     'progress_status_id' => 2,
                     'progress_job_date_start' => Carbon::now()->format('Y-m-d H:i:s'),
                     'progress_job_date_completion' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]);
+            } else if($i == 6) {
+                array_push($arr, [
+                    'job_id' => 2,
+                    'job_sheet_id' => $i,
+                    'engineer_id' => 1,
+                    'management_id' => 1,
+                    'progress_status_id' => 1,
+                    'progress_job_date_start' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'progress_job_date_completion' => null,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]);
+            } else if($i == 7) {
+                array_push($arr, [
+                    'job_id' => 2,
+                    'job_sheet_id' => $i,
+                    'engineer_id' => 1,
+                    'management_id' => 1,
+                    'progress_status_id' => 3,
+                    'progress_job_date_start' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'progress_job_date_completion' => null,
                     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                     'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
                 ]);
