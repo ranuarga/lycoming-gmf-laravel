78bf822954274977cd7e362b58d1d6db163365a4
ranuarga <august.arga@gmail.com>
2020-03-12 10:03:39 +0700

    Update: Seeder Engine Model

--- database/seeds/EngineModelsTableSeeder.php
+++ database/seeds/EngineModelsTableSeeder.php
@@ -13,19 +13,31 @@ class EngineModelsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('engine_models')->insert([
-            [
-                'engine_model_name' => 'O-320',
+        $arr = [];
+        $lines = file(public_path() . '/engine_models.csv');
+        foreach ($lines as $line) {
+            array_push($arr, [
+                'engine_model_name' => $line,
                 'engine_model_reference' => 'OHM 60294-7-14',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ],
-            [
-                'engine_model_name' => 'IO-360',
-                'engine_model_reference' => 'OHM 60294-7-14',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ]
-        ]);
+            ]);
+        }
+
+        DB::table('engine_models')->insert($arr);
+        // DB::table('engine_models')->insert([
+        //     [
+        //         'engine_model_name' => 'O-320',
+        //         'engine_model_reference' => 'OHM 60294-7-14',
+        //         'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        //         'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        //     ],
+        //     [
+        //         'engine_model_name' => 'IO-360',
+        //         'engine_model_reference' => 'OHM 60294-7-14',
+        //         'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        //         'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        //     ]
+        // ]);
     }
 }
