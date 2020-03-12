2cfb9a8c8f1ef2c2fda816f1a1c6e10b3ec174c0
ranuarga <august.arga@gmail.com>
2020-03-12 10:08:30 +0700

    Update: Seeder Engine Model & Job

--- database/seeds/JobsTableSeeder.php
+++ database/seeds/JobsTableSeeder.php
@@ -15,7 +15,7 @@ class JobsTableSeeder extends Seeder
     {
         DB::table('jobs')->insert([
             [
-                'engine_model_id' => 1,
+                'engine_model_id' => 10,
                 'job_order_id' => 1,
                 'job_number' => sprintf("%06d", 1),
                 'job_engine_number' => 'RL-14572-39 A',
@@ -25,7 +25,7 @@ class JobsTableSeeder extends Seeder
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
-                'engine_model_id' => 2,
+                'engine_model_id' => 21,
                 'job_order_id' => 1,
                 'job_number' => sprintf("%06d", 2),
                 'job_engine_number' => 'RL-14272-39 A',
@@ -35,7 +35,7 @@ class JobsTableSeeder extends Seeder
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
-                'engine_model_id' => 2,
+                'engine_model_id' => 23,
                 'job_order_id' => 1,
                 'job_number' => sprintf("%06d", 3),
                 'job_engine_number' => 'RL-14372-39 A',
