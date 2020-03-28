69c9e27872377f1203f88b2e3559c3097f7320c9
ranuarga <august.arga@gmail.com>
2020-03-28 21:28:08 +0700

    Update: Try Handle Load Asset if Production Not Provided With SSL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') !== 'local') {
+        // Force Load Assets in HTTPS (Not Good If My Custom Domain Not Provided With SSL)
+        // if (env('APP_ENV') !== 'local') {
+        //     $this->app['request']->server->set('HTTPS', true);
+        // }
+        
+        // Instead I Try To Use This
+        if (
+            isset($_SERVER['HTTPS']) && 
+            ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || 
+            isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  
+            $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'
+            ) {
             $this->app['request']->server->set('HTTPS', true);
         }
+        // Handle Bug MySQL
         // Schema::defaultStringLength(191);
     }
 }
